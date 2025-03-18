1ce678826a76f4d364d6bb67854768100e9e67e1
LeThinhPhat <lethinhphat7102003@gmail.com>
2025-03-18 16:17:11 +0700

    update

--- src/pages/publisher/ListCampaignPublisher.jsx
+++ src/pages/publisher/ListCampaignPublisher.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router";
 import axiosInstance from "../../lib/axiosInstance";
 import { useQuery } from "@tanstack/react-query";
